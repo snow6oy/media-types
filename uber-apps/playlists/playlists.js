e5db8974b8e4475a706530bd8883e9aa13af1a68
snow6oy <snow6oy@fnarg.net>
2014-03-17 12:08:55 +0100

    playlists.js is the client of the uber api

--- uber-apps/playlists/playlists.js
+++ uber-apps/playlists/playlists.js
@@ -1,19 +1,16 @@
 $(document).ready(
-  //When DOM loaded we attach click event to button, after button is clicked we download the data
+  /* after button is clicked fetch the playlist */
   $('.button').click(function(){
     //start ajax request
     $.ajax({
       url: "/playlists/",
-      //force to handle it as text
       dataType: "text",
       success: function(data) {
-        //data downloaded so we call parseJSON function 
-        //and pass downloaded data
         var json = $.parseJSON(data);
-        //now json variable contains data in json format
         var sources=captions='';
-        /* ignore links for now */
+        /* uber structures are a bit obtuse: i want to think of posterLink as json.uber.playlist[i].url */
         var posterLink = json.uber.data[0].data[0].url;
+        /* ignore the uber links go straight for the playlist data */
 	var uber = json.uber.data[1];
 	uber.data.forEach(function(playlist) {
 	  if (playlist.id==undefined) { 
@@ -22,14 +19,6 @@ $(document).ready(
             captions += '<a href='+ p[1]+ '><img src='+ p[5]+ ' alt="'+ p[7]+ '"></a>';
 	  }
 	});
-
-/*
-        for(var i=0;i<json.playlist.length;i++){
-          sources += '<source src='+ json.playlist[i].source+ ' type='+ json.playlist[i].type+ '>';
-          captions += '<a href='+ json.playlist[i].source+ '><img src='+ json.playlist[i].caption+ ' alt="'+ json.playlist[i].text+ '"></a>';
-        }
-        $('#video_player').html('<video controls poster='+ json.poster+ '>'+ sources+ '</video>'+ '<figcaption>'+ captions+ '</figcaption>');
-*/
         $('#video_player').html('<video controls poster='+ posterLink+ '>'+ sources+ '</video>'+ '<figcaption>'+ captions+ '</figcaption>');
         handleLinks();
       }
@@ -49,8 +38,6 @@ function handler(e){
   e.preventDefault();videotarget=this.getAttribute("href");
   filename=videotarget.substr(0,videotarget.lastIndexOf('.'))||videotarget;
   video=document.querySelector("#video_player video");
-  // video.removeAttribute("controls");
-  // video.setAttribute("poster","http://demosthenes.info/assets/images/video-placeholder.png");
   source=document.querySelectorAll("#video_player video source");
   source[0].src=filename+".mp4";
   source[1].src=filename+".webm";
